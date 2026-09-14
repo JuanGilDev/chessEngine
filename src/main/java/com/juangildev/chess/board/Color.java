58bb4b657262d1ef010f6fa889d31c3c69a8cfa9
Juan <264833738+JuanGilDev@users.noreply.github.com>
2026-09-14 22:07:05 +0200

    Add color and piece enums classes

--- src/main/java/com/juangildev/chess/board/Color.java
+++ src/main/java/com/juangildev/chess/board/Color.java
@@ -1,4 +1,17 @@
 package com.juangildev.chess.board;
 
 public class Color {
+
+    public enum PieceColor {
+        WHITE,
+        BLACK;
+
+        public PieceColor opposite() {
+            if (this == WHITE) {
+                return BLACK;
+            } else {
+                return WHITE;
+            }
+        }
+    }
 }
--- src/main/java/com/juangildev/chess/board/Piece.java
+++ src/main/java/com/juangildev/chess/board/Piece.java
@@ -1,4 +1,33 @@
 package com.juangildev.chess.board;
 
 public class Piece {
+
+    //Un enum puede tener directamente funciones (en este caso getters) y atributos (en este caso value y symbol)
+
+    public enum PieceType {
+        PAWN(100, 'P'),
+        KNIGHT(300, 'N'),
+        BISHOP(300, 'B'),
+        ROOK(500, 'R'),
+        QUEEN(1000, 'Q'),
+        KING(1000000, 'K');
+
+        private final int value;
+        private final char symbol;
+
+        PieceType(int value, char symbol) {
+            this.value = value;
+            this.symbol = symbol;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public char getSymbol() {
+            return symbol;
+        }
+    }
+
+
 }
